bc2dab6974fd92ec55772994d4971fc5f2c7387b
Your Name <you@example.com>
2025-08-20 20:39:29 +0545

    changes

--- src/Component/HeroSection/HeroSection.jsx
+++ src/Component/HeroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ const HeroSection = () => {
     <div className="m-2 md:m-7 md:h-[700px] relative ">
       <div className=" md:h-[700px]  flex items-center overflow-hidden    ">
         <video
-          src="/hero-video.mp4"
+          src="/bgvideooo.mp4"
           className="w-full rounded-xl overflow-hidden mask-cover"
           muted
           autoPlay
@@ -12,7 +12,7 @@ const HeroSection = () => {
         <div className="text-glow absolute p-2 md:p-10">
           <div className="text-xl md:text-4xl lg:text-6xl font-bold text-white ">
             <p className="m-0 p-0">Order your</p>
-            <p className="m-0 p-0">favorite Food here</p>
+            <p className="m-0 p-0">favorite Food here !!</p>
           </div>
           <div className="md:w-[60%] text-[8px] md:text-xl font-medium text-white mt-1 md:mt-5">
             "Discover fresh, delicious meals and ingredients delivered right to
